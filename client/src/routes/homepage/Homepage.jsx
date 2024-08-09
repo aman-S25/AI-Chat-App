3a76065def6032b452214704029ebfc542dee1a8
aman-S25 <aman_k@ph.iitr.ac.in>
2024-08-09 10:37:15 +0530

    Initial commit of AI chat application

---
+++ client/src/routes/homepage/Homepage.jsx
@@ -0,0 +1,83 @@
+import { Link } from "react-router-dom";
+import "./homepage.css";
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
+
+const Homepage = () => {
+  const [typingStatus, setTypingStatus] = useState("human1");
+
+  return (
+    <div className="homepage">
+      <img src="/orbital.png" alt="" className="orbital" />
+      <div className="left">
+        <h1>CHAT AI</h1>
+        <h2>Maximize your creativity and performance</h2>
+        <h3>
+          Experience the future of interaction with our AI-powered chat
+          application. Seamlessly blend creativity and productivity as you
+          engage with intelligent, responsive conversations that elevate your
+          work and ideas.
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className="bot" />
+          <div className="chat">
+            <img
+              src={
+                typingStatus === "human1"
+                  ? "/human1.jpeg"
+                  : typingStatus === "human2"
+                  ? "/human2.jpeg"
+                  : "bot.png"
+              }
+              alt=""
+            />
+            <TypeAnimation
+              sequence={[
+                "Human: We produce food for Mice",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot: We produce food for Hamsters",
+                2000,
+                () => {
+                  setTypingStatus("human2");
+                },
+                "Human2: We produce food for Guinea Pigs",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot: We produce food for Chinchillas",
+                2000,
+                () => {
+                  setTypingStatus("human1");
+                },
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
